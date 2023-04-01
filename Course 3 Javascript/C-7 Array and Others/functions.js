3e6516c690b72db77913a147aeee7c7ec8afbd3c
Abraham Linkon <37959190+linkon63@users.noreply.github.com>
2023-04-01 23:12:44 +0600

    vanila js added

---
+++ Course 3 Javascript/C-7 Array and Others/functions.js
@@ -0,0 +1,76 @@
+// // ✅✅✅ topic 30 : error
+
+// try {
+//     console.log("Hello JS . . . ")
+// } catch (error) {
+//     console.log("Error", error)
+// }
+
+// const errorFind = (e) => {
+//     try {
+//         e + el
+//         console.log(e)
+//         console.log("error find function called")
+//     } catch (error) {
+//         console.log("✅ error", error)
+//     }
+// }
+
+// errorFind(10)
+// // ✅✅✅ topic 31 : async 1
+
+// console.log("1")
+// console.log("2")
+// setTimeout(() => {
+//     console.log("🔄️ 3")
+//     console.log("🔄️ 4")
+// }, 5000) // 1 second = 1000 ms
+// console.log("5")
+// console.log("6")
+// console.log("7")
+
+// wall e ?
+
+// // ✅✅✅ topic 32 : async 2
+
+// let count = 0;
+// setInterval(function () {
+//     console.log("7")
+// }, 2000)
+
+// // ✅✅✅ topic 33 : async 3
+
+// setTimeout(() => {
+//     console.log("✅ I will run at outside")
+// })
+// console.log("✅ I will see Instead")
+// setTimeout(() => {
+//     console.log("✅ I will see after 1 second")
+//     setTimeout(() => {
+//         console.log("✅ I will see after 2 second")
+//     }, 2000)
+// }, 1000)
+// console.log("✅ I will see end")
+
+// setTimeout(() => {
+//     console.log("✅ I will run at outside 1")
+// }, 1000)
+
+// for (let i = 0; i < 5; i++) {
+//     console.log("I :", i)
+// }
+
+// setTimeout(() => {
+//     console.log("✅ I will run at outside 2")
+// }, 1000)
+
+// for (let i = 0; i < 5; i++) {
+//     console.log("J :", i)
+// }
+
+
+
+
+
+
+
